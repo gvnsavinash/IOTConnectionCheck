1db134155d9a404d44421c0cc5a4bb3ff6d31e5e
gvnsavinash <139457108+gvnsavinash@users.noreply.github.com>
2024-12-22 14:22:09 -0500

    Update Function1.cs

--- IotAndSqlConnectionChecking/Function1.cs
+++ IotAndSqlConnectionChecking/Function1.cs
@@ -28,7 +28,7 @@ namespace IotAndSqlConnectionChecking
         private static HttpClient client = new HttpClient();
 
         [FunctionName("Function1")]
-        public static void Run([IoTHubTrigger("sql-pre-ue1-prd-mc",ConsumerGroup ="test", Connection = "Conn")] EventData message, ILogger log)
+        public static void Run([IoTHubTrigger("sql-pres-ue1s-prsd-msc",ConsumerGroup ="test", Connection = "Conn")] EventData message, ILogger log)
         {
             log.LogInformation($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
 
@@ -36,16 +36,18 @@ namespace IotAndSqlConnectionChecking
             {
                 //parsing the input json data
 
+                ## Note All the Server names and UserID and Passwords are Fictional 
+
                 // Providing SQL credentials
-                //string strcon = @"Data Source =sql-pre-ue1-prd-mc.database.windows.net;Trusted_Connection=no;Authentication=Active Directory Password;Database=DB-PRE-UE1-PRD-MC;User ID=sponnana@pregis.com;Password=India@2022;";
+                //string strcon = @"Data Source =sql-pres-ue1s-prsd-msc.database.windows.net;Trusted_Connection=no;Authentication=Active Directory Password;Database=DatabaseName;User ID=UserID;Password=India@2022;";
 
                  //----SQL SERVER Authentication Connection Strings---
-                //string strcon = @"Server = tcp:sql - pre - ue1 - prd - mc.database.windows.net,1433; Initial Catalog = DB - PRE - UE1 - PRD - MC; Persist Security Info = False; User ID = PregisAdmin; Password ={ your_password}; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
-                //string strcon = @"Server=tcp:sql-pre-ue1-prd-mc.database.windows.net;Initial Catalog=DB-PRE-UE1-PRD-MC;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                //string strcon = @"Server = tcp:sql - pre - ue1 - prd - mc.database.windows.net,1433; Initial Catalog = DB - PRE - UE1 - PRD - MC; Persist Security Info = False; User ID = UserID; Password ={ your_password}; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
+                //string strcon = @"Server=tcp:sql-pres-ue1s-prsd-msc.database.windows.net;Initial Catalog=DatabaseName;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
                 //------------------
 
-                string strcon = @"Server=tcp:sql-pre-ue1-prd-mc.database.windows.net,1433;Initial Catalog=DB-PRE-UE1-PRD-MC;Persist Security Info=False;User ID=SQLPortalFA@pregis.com;Password=Aflqslatrop20@#!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication=Active Directory Password;";
-                //string strcon = @"Data Source =sql-pre-ue1-prd-mc.database.windows.net;Trusted_Connection=no;Authentication=Active Directory Password;Database=DB-PRE-UE1-PRD-MC;User ID=ag@pregis.com;Password=@OnceUponATime!;";
+                string strcon = @"Server=tcp:sql-pres-ue1s-prsd-msc.database.windows.net,1433;Initial Catalog=DatabaseName;Persist Security Info=False;User ID=;Password=Aflqslatrop20@#!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication=Active Directory Password;";
+                //string strcon = @"Data Source =sql-pres-ue1s-prsd-msc.database.windows.net;Trusted_Connection=no;Authentication=Active Directory Password;Database=DatabaseName;User ID=UserID;Password=@OnceUponATime!;";
 
                 using (SqlConnection con = new SqlConnection(strcon))
                 {
